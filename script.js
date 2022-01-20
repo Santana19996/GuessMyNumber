e52e60890645de92fe63a763f026153ce58dc9b6
Oscar <oscarsantana2014@gmail.com>
2022-01-20 14:25:36 -0600

    Check for value

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 "use strict";
 
-document.querySelector(".message");
+// console.log(document.querySelector(".message").textContent);
+
+// // document.querySelector(".message").textContent = "Hello from Oscar ";
+
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
+
+// document.querySelector(".guess").value = 23;
+// console.log(document.querySelector(".guess").value);
+
+document.querySelector(".check").addEventListener("click", function (event) {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number entered! ";
+  }
+});
